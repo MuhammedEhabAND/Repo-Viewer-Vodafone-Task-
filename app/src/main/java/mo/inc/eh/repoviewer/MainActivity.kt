e875b3961b9599e0d9a370bfcaf3b0bf20da9ece
MuhammedEhabAND <muhammedehab726@gmail.com>
2024-01-23 18:48:17 +0200

    adding splash screen

--- app/src/main/java/mo/inc/eh/repoviewer/MainActivity.kt
+++ app/src/main/java/mo/inc/eh/repoviewer/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import mo.inc.eh.repoviewer.ui.theme.RepoViewerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             RepoViewerTheme {
                 // A surface container using the 'background' color from the theme
